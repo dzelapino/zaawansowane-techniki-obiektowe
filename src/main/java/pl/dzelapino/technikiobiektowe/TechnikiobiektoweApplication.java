c4b804fe59f8c373e75a2124176f50b55d7f57d1
dzelapino <kolodziejskikrzysztofhubert@gmail.com>
2024-10-05 10:27:17 +0200

    - Init project

---
+++ src/main/java/pl/dzelapino/technikiobiektowe/TechnikiobiektoweApplication.java
@@ -0,0 +1,13 @@
+package pl.dzelapino.technikiobiektowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnikiobiektoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnikiobiektoweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dzelapino/technikiobiektowe/TechnikiobiektoweApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dzelapino.technikiobiektowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnikiobiektoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
