8e62c3ad18323472538a4a53669fc38ee9e7bd65
dzelapino <kolodziejskikrzysztofhubert@gmail.com>
2024-11-17 14:30:34 +0100

    Widoki komentarze

--- zto/ztoweb/views.py
+++ zto/ztoweb/views.py
@@ -14,10 +14,12 @@ from ztoweb.models import Hero, User, Profile
 def home(req):
     return render(req, 'home.html')
 
+## Lista wyswietlająca bohaterów
 def heroes(request):
     hero_list = Hero.objects.all()
     return render(request, "heroes/hero_list.html", {"heroes":hero_list})
 
+## Chroniony widok tworzenia bohatera
 def create_hero(request):
     context = {}
     form = HeroForm(request.POST or None)
@@ -55,34 +57,21 @@ def register(request):
 def login_view(request):
     context = {}
 
+    ## Jeżeli jest post sprawdź dane logowania
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
+            ## Jeżeli znaleziono usera o odpowiadających danych przekieruj na chroniony widok ctworzena bohatera
             if user is not None:
                 login(request, user)
                 return redirect('create_hero')
     else:
+        ## Jeżeli jest get formularz jest formularzem logowania
         form = AuthenticationForm()
 
+    ## Renderuj stronę logowania
     context['form'] = form
     return render(request, "profiles/profile_login.html", context)
-
-
-
-    # return render(request, "profiles/profile_login.html", context)
-
-    # form = LoginForm(request.POST or None)
-    #
-    # if request.method == "POST":
-    #     if form.is_valid():
-    #         return redirect('home')
-    #
-    # context['form'] = form
-    # return render(request, "profiles/profile_login.html", context)
-
-def logout_view(request):
-    logout(request)
-    return redirect('home')
